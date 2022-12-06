35a522865cf24657f27929aa5b80a84e1bec150b
delmashajan <delmashajan@gmail.com>
2022-12-06 10:56:17 +0530

    firstcommit

---
+++ src/test/java/com/nest/courseapp_backend/CourseappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.courseapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
